e84fc05eedc2041f166a1b086aa32bbd49785da9
SAKTHIVEL M <sakthi492003@gmail.com>
2026-04-11 19:24:07 +0530

    works fine

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ def retrain_model():
     print("--- Retraining Model with XGBoost ---")
     
     # 1. Load Data
-    data_path = "phishing.csv"
+    data_path = "phishing dataset.csv"
     if not os.path.exists(data_path):
         print(f"Error: {data_path} not found.")
         return
